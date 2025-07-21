55e863d5e5dc65cf84f437c227247fdc980fefb9
barsukova13 <barsukovavitebsk13@gmail.com>
2025-07-21 16:07:26 +0300

    commit2

--- notification-service/src/main/java/org/example/NotificationApplication.java
+++ notification-service/src/main/java/org/example/NotificationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEurekaClient
     public class NotificationApplication {
         public static void main(String[] args) {
             SpringApplication.run(NotificationApplication.class, args);
--- user-service/src/main/java/org/example/service/UserService.java
+++ user-service/src/main/java/org/example/service/UserService.java
@@ -2,29 +2,36 @@ package org.example.service;
 
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.example.dto.UserEvent;
 import org.example.dto.UserOperation;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.example.repository.UserRepository;
 
 import org.springframework.stereotype.Service;
 import org.example.model.User;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final KafkaTemplate<String, UserEvent> kafkaTemplate; // Изменили тип
+    private final KafkaTemplate<String, UserEvent> kafkaTemplate;
+    private final ReactiveCircuitBreakerFactory circuitBreakerFactory;// Изменили тип
 
     public void createUser(User user) {
         User savedUser = userRepository.save(user);
-        sendEvent(savedUser.getEmail(), UserOperation.CREATED); // Используем enum
+        sendEventWithCircuitBreaker(savedUser.getEmail(), UserOperation.CREATED)
+                .doOnError(e -> log.error("Failed to send CREATE event for user {}", savedUser.getEmail(), e))
+                .subscribe();
     }
 
     public void deleteUser(Long id) {
         userRepository.findById(id).ifPresent(user -> {
             userRepository.delete(user);
-            sendEvent(user.getEmail(), UserOperation.DELETED);
+            sendEventWithCircuitBreaker(user.getEmail(), UserOperation.DELETED)
+                    .doOnError(e -> log.error("Failed to send DELETE event for user {}", user.getEmail(), e))
+                    .subscribe();
         });
     }
 
